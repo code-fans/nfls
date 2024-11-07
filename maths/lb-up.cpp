ffd9d132e5c659123289b619b57225b9155894f9
codefan <codefan@centit.com>
2024-11-07 17:15:18 +0800

    lower bound upper bound

--- maths/lb-up.cpp
+++ maths/lb-up.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int lb(int ar[], int len, int v){
-    int l=0, r=len;
+    int l=-1, r=len;
     while(l<r-1){
         int mid = (l+r) / 2;
         if(ar[mid] >= v){
@@ -15,7 +15,7 @@ int lb(int ar[], int len, int v){
 }
 
 int ub(int ar[], int len, int v){
-    int l=0, r=len;
+    int l=-1, r=len;
     while(l<r-1){
         int mid = (l+r) / 2;
         if(ar[mid] <= v){
@@ -28,9 +28,11 @@ int ub(int ar[], int len, int v){
 }
 
 int main(){
-    int a[] = {1,2,3,3,3,3,4,5,6,7,7,8,9,10};
-    int l3 = lb(a, 15, 3);
-    int u3 = ub(a, 15, 3);
+    int v;
+    cin >> v;
+    int a[] = {1,1,3,3,3,4,5,6,7,7,8,9,10,12,15};
+    int l3 = lb(a, 15, v);
+    int u3 = ub(a, 15, v);
     cout<< l3 <<' '<< u3 << endl;
     return 0;
 }
