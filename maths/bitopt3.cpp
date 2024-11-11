684c48413fa076cb30c926ae7fd5616e1caed853
Wilbur <yang_qh@126.com>
2024-11-11 22:12:35 +0800

    位运算

--- maths/bitopt3.cpp
+++ maths/bitopt3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <bitset>
+
 using namespace std;
 int main()
 {
@@ -6,8 +8,8 @@ int main()
     cin>>n;
     while (n){
         ans++;
+        cout<< bitset<32>(n) << ' '<< n << '\t' << bitset<32>(n-1) << ' ' << n-1 << endl;
         n&=(n-1);
-        cout<< n << endl;
     }
     cout<<ans<<endl;
     return 0;
