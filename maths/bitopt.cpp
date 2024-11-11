9f42b3af61562e5d5f00dfb50537b81844e17359
codefan <codefan@centit.com>
2024-11-11 09:41:37 +0800

    位运算

--- maths/bitopt.cpp
+++ maths/bitopt.cpp
@@ -5,11 +5,11 @@ void chaifen(int n){
     if(n==0) return;
     int p = n & (-n);
     // chaifen(n-p);
-    // Brian Kernighan算法?
     chaifen(n & (n-1));
     cout << p << ' ';
 }
 
+// Brian Kernighan 算法?  布莱恩·柯林汉 算法
 int bitCount(int n){
     int bs = 0;
     while(n!=0){
@@ -19,11 +19,18 @@ int bitCount(int n){
     return bs;
 }
 
+//汉明距离
 int hammingDistance(int x, int y){
     return bitCount(x ^ y);
 }
 
-int leftmostOne(int n) {
+// 最右侧的 1 
+int rightMostOne(int n) {
+    return n & (-n);
+}
+
+// 最左侧的 1 
+int leftMostOne(int n) {
     // 确保 第一个 1 后面的所有位 都是 1
     n |= (n >> 1);
     n |= (n >> 2);
@@ -33,11 +40,12 @@ int leftmostOne(int n) {
     return n - (n >> 1);
 }
 
+
 int main(){
     int n=12356;
     chaifen(n);
     cout << endl;
-    cout << leftmostOne(n) << endl;
+    cout << leftMostOne(n) << endl;
     cout << bitCount(n) << endl;
     cout << hammingDistance(n, n-leftmostOne(n)) << endl;
     return 0;
