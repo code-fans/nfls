ebfe914773a4b591b35f7bc1615dd55d11d1d021
codefan <codefan@centit.com>
2024-12-10 09:28:03 +0800

    fix it

--- csp2126/problem3.cpp
+++ csp2126/problem3.cpp
@@ -17,12 +17,10 @@ int main(){
     }
     int b = (1 << 16) + (1 << 1);
     const int e = (R << 16) + (1<<R);
-    int step = 1;
     queue <pair<int, int> > qu;
     set<int> foot;
     qu.push({b, 0});
     foot.insert(b);
-
     while(!qu.empty()){
         pair<int, int> st = qu.front();
         int c = st.first;
@@ -33,26 +31,23 @@ int main(){
             if(kg[i][0] == r){
                 int next = (r << 16) + (s ^ (1 << kg[i][1]));
                 if(next == e){
-                    cout << "Mr. Black needs " << step << " steps."<< endl;
+                    cout << "Mr. Black needs " << st.second+1 << " steps."<< endl;
                     return 0;
                 }
-            
                 if(foot.count(next) == 0){
                     qu.push({next, st.second+1});
                     foot.insert(next);
                 }
             }
         }
-
         for(int i=0; i<D; i++){
             if(edge[i][0] == r){
-                int next = (edge[i][1] << 16)+ s;
+                int next = (edge[i][1] << 16) + s;
                 if(foot.count(next) == 0){
                     qu.push({next, st.second+1});
                     foot.insert(next);
                 }
             }
-
             if(edge[i][1] == r){
                 int next = (edge[i][0] << 16)+ s;
                 if(foot.count(next) == 0){
@@ -61,8 +56,8 @@ int main(){
                 }
             }
         }
-
     }
+
     cout << "Poor Mr. Black! No sleep tonight!" << endl;
     return 0;
 }
